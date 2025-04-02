1ee469e5ee8883e651cb17a0acba77105e2b87b4
ily1s <101256242+ily1s@users.noreply.github.com>
2025-04-02 16:47:40 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+
+# Load the .env file
+load_dotenv()
+
+# Access the credentials
+consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
+consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET")
+access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+
+# Use the credentials (e.g., with Tweepy for Twitter API)
+import tweepy
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+print("Connected to Twitter API!")
