96bc40ee720e85ef19fe3e3b7384635d6b85fabf
ily1s <101256242+ily1s@users.noreply.github.com>
2025-04-03 19:46:53 +0000

    commited

--- src/g.py
+++
@@ -1,6 +0,0 @@
-import h5py
-try:
-    with h5py.File('/Users/ilyas/myenv/YARBI/AI4SDG/models/model_v2.weights.h5', 'r') as f:
-        print("File loaded successfully!")
-except OSError as e:
-    print(f"Error: {e}")
--- src/hala.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-print(tf.__version__)
-from keras.preprocessing.sequence import pad_sequences
-print("✅ Keras import works!")
-
